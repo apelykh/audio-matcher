1a885183792e830bfccaf46fef99890fa8fda159
Anton Pelykh <anton.pelykh@gmail.com>
2020-04-19 14:31:08 +0100

    Processing query files from folder implemented

--- audio_matcher.py
+++ audio_matcher.py
@@ -1,16 +1,3 @@
-# To aid in automatic testing, your code must be callable in two parts as follows:
-#     fingerprintBuilder(/path/to/database/, /path/to/fingerprints/)
-#     audioIdentification(/path/to/queryset/, /path/to/fingerprints/, /path/to/output.txt)
-#
-# The format of the output.txt file will include one line for each query audio recording, using the
-# following format:
-# query audio 1.wav database audio x1.wav database audio x2.wav database audio x3.wav
-# query audio 2.wav database audio y1.wav database audio y2.wav database audio y3.wav
-# ...
-# where the filenames in each line are separated by a tab, and the three database recordings per line are
-# the top three ones which are identified by the audio identification system as more closely matching
-# the query recording, ranked from first to third.
-
 import os
 import numpy as np
 import time
@@ -22,37 +9,55 @@ from fingerprinting import fingerprint
 class AudioMatcher:
     def __init__(self):
         self.id_to_song = {}
+        self.song_to_id = {}
         self.fingerprints_db = {}
 
-    def build_database(self, audio_folder_path, pickle_path=None):
-        if pickle_path:
-            # load the database from file
-            with open(pickle_path, 'r') as f:
+    def _build_song_mappings(self, audio_folder_path: str):
+        """
+        Build self.id_to_song mapping when the database is loaded from file.
+
+        :param audio_folder_path:
+        """
+        for i, audio_file in enumerate(sorted(os.listdir(audio_folder_path))):
+            self.id_to_song[i] = audio_file
+        self.song_to_id = {v: k for k, v in self.id_to_song.items()}
+
+    def build_database(self, audio_folder_path: str, db_filepath: str = './data/fingerprints_db.pkl'):
+        if os.path.isfile(db_filepath):
+            print('[.] Loading the databse...')
+
+            with open(db_filepath, 'rb') as f:
                 self.fingerprints_db = pickle.load(f)
+            self._build_song_mappings(audio_folder_path)
         else:
-            # build the database
-            for i, audio_file in enumerate(os.listdir(audio_folder_path)):
-                print(audio_file)
+            print('[.] Building the databse...')
+
+            # sort the files to ensure consistent order
+            for i, audio_file in enumerate(sorted(os.listdir(audio_folder_path))):
+                if not audio_file[-4:] == '.wav':
+                    continue
+
+                if i % 30 == 0:
+                    print('File {}/{}'.format(i, len(os.listdir(audio_folder_path))))
                 self.id_to_song[i] = audio_file
+                self.song_to_id[audio_file] = i
 
                 audio_path = os.path.join(audio_folder_path, audio_file)
-                s1 = time.time()
                 audio, sr = librosa.load(audio_path)
-                read_time = time.time() - s1
 
-                s2 = time.time()
+                # s2 = time.time()
                 song_peaks_map = fingerprint(audio, song_id=i)
-                finger_time = time.time() - s2
-                print("--- read: {}, fingerprint: {} ---".format(read_time, finger_time))
+                # finger_time = time.time() - s2
+                # print("--- fingerprinting time: {} ---".format(finger_time))
 
                 self.fingerprints_db.update(song_peaks_map)
 
-                if i == 10:
-                    break
+            with open(db_filepath, 'wb') as f:
+                pickle.dump(self.fingerprints_db, f)
 
         print('DB contains {} fingerprints'.format(len(self.fingerprints_db)))
 
-    def match_song(self, song_path: str, num_results=3):
+    def match_song(self, song_path: str, num_results=3) -> tuple:
         audio, sr = librosa.load(song_path)
         song_peaks_map = fingerprint(audio)
 
@@ -63,17 +68,15 @@ class AudioMatcher:
             if feature in self.fingerprints_db:
                 db_song_offset, song_id = self.fingerprints_db[feature]
                 query_song_offset = song_peaks_map[feature][0]
-
                 time_diff = db_song_offset - query_song_offset
 
                 if song_id not in id_to_time_diffs:
                     id_to_time_diffs[song_id] = []
-                # id_to_time_diffs[song_id].append((db_song_offset, query_song_offset))
                 id_to_time_diffs[song_id].append(time_diff)
 
+        # count the frequencies of unique time differences for each song
         song_ids = []
         max_counts = []
-        # count unique time differences for each song id
         for song_id, time_diffs in id_to_time_diffs.items():
             _, counts = np.unique(time_diffs, return_counts=True)
             max_counts.append(int(np.max(counts)))
@@ -81,12 +84,42 @@ class AudioMatcher:
 
         res_indices = np.argsort(max_counts)[::-1][:num_results]
 
-        return [self.id_to_song[song_ids[i]] for i in res_indices]
+        return tuple(song_ids[i] for i in res_indices)
+
+    def match_from_folder(self, queries_folder: str, output_filepath='./results.txt') -> dict:
+        results = {}
+
+        if output_filepath:
+            f = open(output_filepath, 'w')
+
+        for i, audio_file in enumerate(sorted(os.listdir(queries_folder))):
+            if not audio_file[-4:] == '.wav':
+                continue
+
+            gt_song_name = '{}.wav'.format(audio_file.split('-')[0])
+            gt_song_id = self.song_to_id[gt_song_name]
+
+            audio_path = os.path.join(queries_folder, audio_file)
+            matched_ids = self.match_song(audio_path)
+            results[gt_song_id] = matched_ids
+
+            if output_filepath:
+                song_names = [self.id_to_song[song_id] for song_id in matched_ids]
+                res_string = '{}\t{}\t{}\t{}\n'.format(gt_song_name, *song_names)
+                f.write(res_string)
+
+        if output_filepath:
+            f.close()
+
+        return results
 
 
 if __name__ == '__main__':
     matcher = AudioMatcher()
-    matcher.build_database('./data/database_recordings')
+    matcher.build_database('./data/database_recordings', './data')
     print('-' * 40)
-    matches = matcher.match_song('./data/query_recordings/pop.00049-snippet-10-20.wav')
-    print(matches)
+    # matches = matcher.match_song('./data/query_recordings/pop.00049-snippet-10-0.wav')
+    # matches = matcher.match_song('./data/query_recordings/classical.00014-snippet-10-20.wav')
+    # print(matches)
+    results = matcher.match_from_folder('./data/query_recordings')
+    print(results)
